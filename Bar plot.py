cebeaf98ab795fa0c9e388ea139553932d0b8e9f
BibiFatimaAyaz <93053293+BibiFatimaAyaz@users.noreply.github.com>
2021-10-29 21:34:27 +0500

    Add files via upload

---
+++ Bar plot.py
@@ -0,0 +1,13 @@
+#_________-BAR PLOT-__________
+
+import matplotlib.pyplot as plt
+
+students=['ASMA','AREEBA','ABEEHA','ALEENA']
+marks=[88,93,45,60]
+
+
+plt.bar(students,marks)
+
+#plt.barh(students,marks)    #this H is for drawing graph horizontally
+
+plt.show()
